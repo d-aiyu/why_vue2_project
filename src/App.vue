718dc8a3e75e9c9e19c05e4c0cc0ae3c88a61241
d-aiyu <2637003019@qq.com>
2022-06-07 00:47:46 +0800

    商城项目文件结构初始化 创建底部导航

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <hY-tabbar></hY-tabbar>
+</template>
+
+
+
+<script>
+import HYTabbar from './components/common/HYTabbar.vue'
+export default {
+  // name: "App",
+  components:{
+    HYTabbar,
+  }
+};
+</script>
+
+<style>
+@import url('./assets/css/base.css');
+
+</style>
---
+++ src/components/common/HYTabbar.vue
@@ -0,0 +1,34 @@
+<template>
+<div id="tab-bar" >
+  <div class="tab-bar">首页</div>
+  <div class="tab-bar">分类</div>
+  <div class="tab-bar">购物车</div>
+  <div class="tab-bar">我的</div>
+</div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+#tab-bar{
+  display: flex;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+.tab-bar{
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  line-height: 49px;
+  box-shadow: 0 -1px 0 0 rgba(66,66,66,0.1);
+  
+
+
+}
+</style>
